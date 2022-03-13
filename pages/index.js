88b966dc058c58400306874176ea5bcacce81214
Thuy Pham <thuy.pham@eastgate-software.com>
2022-03-13 09:21:13 +0700

    create initia; project

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import userData from "@constants/data";
+import getLatestRepos from "@lib/getLatestRepos";
+import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import Hero from "../components/Hero";
+import LatestCode from "../components/LatestCode";
+
+export default function Home({ repositories }) {
+  return (
+    <ContainerBlock
+      title="Manu Arora - Developer, Writer, Creator"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      <FavouriteProjects />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
+}
+
+export const getServerSideProps = async () => {
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
